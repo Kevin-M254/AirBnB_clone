d70ad89383b8410cfea63111a027e51011a50ba3
Kevin-M254 <kmburu007@gmail.com>
2023-02-12 15:59:07 +0300

    base model for AirBnB clone

---
+++ models/engine/file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Class for storing and retreiving data"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
