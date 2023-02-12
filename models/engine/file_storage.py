9ec517da5500ab2f500e23d5127ddd0fdf55608a
Kevin-M254 <kmburu007@gmail.com>
2023-02-12 20:25:32 +0300

    update command interpreter

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,3 +25,31 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+
+        classes = {"BaseModel": BaseModel}
+        return classes
+
+    def reload(self):
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+                "BaseModel":
+                         {"id": str,
+                          "created_at": datetime.datetime,
+                          "updated_at": datetime.datetime}
+                }
+        return attributes
